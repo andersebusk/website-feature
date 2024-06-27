4b8eca5f97a52dd47fd20f442a276898e1676851
Andersebusk <s235194@dtu.dk>
2024-06-27 10:39:16 +0200

    "Updated front page"

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to MFT&apos;s Calculator.</strong> This is where you will begin reducing oil and energy consumption and save money!{' '}
+            <strong>Welcome to MFT&apos;s Savings Calculator.</strong> This is where you will begin reducing oil and energy consumption and save money!{' '}
             <a href="https://marinefluid.dk/" className="text-customColor2">
               Learn more about our company here.{' '}
             </a>
