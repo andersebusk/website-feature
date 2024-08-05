9f1f3e2e250ceb6d39df014d7d1d681b6886a77b
Andersebusk <s235194@dtu.dk>
2024-08-05 12:28:07 +0200

    "download message"

--- app/downloadsite/page.tsx
+++ app/downloadsite/page.tsx
@@ -191,7 +191,7 @@ const App: React.FC = () => {
           </div>
         </header>
         <footer className={styles.footer}>
-          Congratulations! You are taking the first step towards saving both the planet and money! <br />
+          Congratulations! You are taking the first step towards a helthier planet and economy! <br />
           Do not hesitate to contact us for a follow up meeting, to discuss the opportunities in greater details.
         </footer>
         <main className={styles.main}>
